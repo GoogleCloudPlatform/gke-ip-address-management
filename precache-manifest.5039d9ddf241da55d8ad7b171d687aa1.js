da59cb1758025a41b95a372d6445e711cb4f4ea0
Andres Olarte <andresolarte@google.com>
2019-11-27 17:12:22 -0600

    First commit

---
+++ precache-manifest.5039d9ddf241da55d8ad7b171d687aa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9cd023896f8eb98008f4e59915ecf20",
+    "url": "/gke-ip-address-management/index.html"
+  },
+  {
+    "revision": "7f40f84dee8a721ae9c6",
+    "url": "/gke-ip-address-management/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "b1955804eeb404882d67",
+    "url": "/gke-ip-address-management/static/css/main.9549d78d.chunk.css"
+  },
+  {
+    "revision": "7f40f84dee8a721ae9c6",
+    "url": "/gke-ip-address-management/static/js/2.cb006032.chunk.js"
+  },
+  {
+    "revision": "b1955804eeb404882d67",
+    "url": "/gke-ip-address-management/static/js/main.86fc8b49.chunk.js"
+  },
+  {
+    "revision": "0b48c02aca950fc4e2d5",
+    "url": "/gke-ip-address-management/static/js/runtime~main.f57fdad4.js"
+  }
+]);
