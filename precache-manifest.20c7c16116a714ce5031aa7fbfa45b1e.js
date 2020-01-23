93119a49117f1695bdd466e7cb92aa9e8b996285
aolarte <aolarte@users.noreply.github.com>
2020-01-23 17:27:41 +0000

    Automated deployment: Thu Jan 23 17:27:41 UTC 2020 e3d656e55d0a85a7d93aa7776fae63aa2be84ac4

---
+++ precache-manifest.20c7c16116a714ce5031aa7fbfa45b1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79de9459e70aed020bdc3a06804c8f36",
+    "url": "/gke-ip-address-management/index.html"
+  },
+  {
+    "revision": "ccc66048855c45fa6c3f",
+    "url": "/gke-ip-address-management/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "89a7166d6edb1cd6f4d1",
+    "url": "/gke-ip-address-management/static/css/main.9549d78d.chunk.css"
+  },
+  {
+    "revision": "ccc66048855c45fa6c3f",
+    "url": "/gke-ip-address-management/static/js/2.a6a4da62.chunk.js"
+  },
+  {
+    "revision": "89a7166d6edb1cd6f4d1",
+    "url": "/gke-ip-address-management/static/js/main.73e2a41f.chunk.js"
+  },
+  {
+    "revision": "0b48c02aca950fc4e2d5",
+    "url": "/gke-ip-address-management/static/js/runtime~main.f57fdad4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2dea31c7393a0dc0841b2a9a87a83fd.js"
+  "/gke-ip-address-management/precache-manifest.20c7c16116a714ce5031aa7fbfa45b1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gke-ip-address-management/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
